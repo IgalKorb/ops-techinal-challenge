f187c107d568fb6dad21244b8558d44b7089c755
Igal Korb <igalkorb@Igals-MBP.lan>
2020-05-01 13:21:50 +0300

    Exerceise5

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo apt-get install sshpass
+sshpass -f <(printf '%s\n' vagrant) ssh -o "StrictHostKeyChecking=no" 192.168.100.10 'cat /home/vagrant/.ssh/id_rsa.pub' | cat >> /home/vagrant/.ssh/authorized_keys
+echo "StrictHostKeyChecking no" > /home/vagrant/.ssh/config
+ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa
+sed 's/root/vagrant/' /home/vagrant/.ssh/id_rsa.pub | sudo tee /home/vagrant/.ssh/id_rsa.pub
+sudo chown vagrant:vagrant /home/vagrant/.ssh/id_rsa* /home/vagrant/.ssh/config
