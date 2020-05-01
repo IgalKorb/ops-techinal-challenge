05536ab7471facb9fc7e12ad7ce652048317cbcd
Igal Korb <igalkorb@Igals-MBP.lan>
2020-05-01 13:21:14 +0300

    Exerceise3

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise3 here
+sed '13 s/deny/allow/' /etc/apache2/sites-enabled/000-default | sudo tee /etc/apache2/sites-enabled/000-default ; sudo service apache2 restart
